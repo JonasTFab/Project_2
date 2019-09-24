0a88d9e8cd48bbc335c54496fe03cd47f0ea6005
JonasTFab <54434853+JonasTFab@users.noreply.github.com>
2019-09-24 14:36:50 +0200

    Ch ch ch changes

--- eigen_value_problems/main.cpp
+++ eigen_value_problems/main.cpp
@@ -15,7 +15,7 @@ double lambda_analytical(int N, double j,double d,double a){
 }
 
 
-int jacobis_method(arma::mat A,double d, double a, int n){
+int jacobis_method(arma::mat A, arma::Col<double> d, arma::Col<double> a, int n){
     double tol = 10e-8;
     double max = 2*tol;                // just to make max a little bigger than the tolerance before running the while loop
     int k,l,iter,iter_max;
@@ -24,7 +24,7 @@ int jacobis_method(arma::mat A,double d, double a, int n){
     iter_max = n*n*n;
 
     // Setting up the eigenvector matrix (R.eye() automatically sets identity matrix)
-    arma::Mat<double> R = arma::mat(n,n); R.eye();
+    arma::Mat<double> R = arma::mat(n,n).eye();
 
     // finds max value of off-diagonal elements assuming symmetric matrix
     while (max > tol && iter < iter_max){
@@ -91,32 +91,58 @@ int jacobis_method(arma::mat A,double d, double a, int n){
         }       // end of test 1
     }       // end of while loop
 
+    // Puts the diagonal elements (eigenvalues) in matrix A into a column vector
     arma::Col<double> e_val = arma::vec(n);
     for (int i=0; i<n; i++){
         e_val(i) = A(i,i);
     }
 
 
-    // test 2: comparing the analytical and numerical eigenvalues
-    arma::Col<double> sorted_e_val, e_val_analytic(n), sorted_e_val_analytic, diff;
-    sorted_e_val = arma::sort(e_val);
-    for(int i=0; i<n; i++){                // analytical calculations
-        e_val_analytic(i) = lambda_analytical(n, i+1, d, a);
-    }
-    sorted_e_val_analytic = arma::sort(e_val_analytic);
-    for (int i=0; i<n; i++){
-        double diff = sorted_e_val(i) - sorted_e_val_analytic(i);
-        if (diff > tol){
-            std::cout << "The difference between the eigenvalues is bigger than the tolerance!" << endl <<
+    // test 2a: comparing the analytical and numerical eigenvalues (works only for a Töplitz matrix)
+    if (d(0)==d(1) && d(0)==d(n-1) && a(0)==a(1) && a(0)==a(n-1)){
+        arma::Col<double> sorted_e_val, e_val_analytic(n), sorted_e_val_analytic, diff;
+        sorted_e_val = arma::sort(e_val);
+        for(int i=0; i<n; i++){                // analytical calculations
+            e_val_analytic(i) = lambda_analytical(n, i+1, d(0), a(0));
+        }
+        sorted_e_val_analytic = arma::sort(e_val_analytic);
+        for (int i=0; i<n; i++){
+            double diff = sorted_e_val(i) - sorted_e_val_analytic(i);
+            if (abs(diff) > tol){
+                std::cout << "The difference between the eigenvalues are bigger than the tolerance!" << endl <<
                          "Index: " << i << "     Analytic eigenvalue: " << sorted_e_val_analytic(i) <<
                          "     Numerical eigenvalue: " << sorted_e_val(i) << "\n\n" ;
         }
-    } // end of test 2
+        }
+    } // end of test 2a
+
+    // test 2b: comparing the analytical and numerical eigenvalues (works for Schrödinger equation)
+    else if (d(0)!=d(1) && d(0)!=d(n-1) && a(0)==a(1) && a(0)==a(n-1)){
+        arma::Col<double> sorted_e_val, e_val_analytic(n), diff;
+        sorted_e_val = arma::sort(e_val);
+        for (int i=0; i<n; i++){
+            e_val_analytic(i) = 3 + i*4;       // assuming there exists a pattern for the eigenvalues
+        }
+        for (int i=0; i<n; i++){
+            double diff = sorted_e_val(i) - e_val_analytic(i);
+            if (abs(diff) > tol){
+                std::cout << "The difference between the eigenvalues are bigger than the tolerance!" << endl <<
+                         "Index: " << i << "     Analytic eigenvalue: " << e_val_analytic(i) <<
+                         "     Numerical eigenvalue: " << sorted_e_val(i) << "\n\n" ;
+        }
+        }
+    } // end of test 2b
+
+    else {
+        arma::cout << "Missing an analytic solution to compare with!" << endl;
+        return 0;
+    }
 
 
     //OUTPUT!!!!!!!!!!!!!!!!
-    //std::cout << "Number of iterations: " << iter << endl ;
-    //std::cout << "Theoretical number of rotations between 3n^2-5n^2: " << 3*pow(n,2)<< "-" << 5*pow(n,2)<< endl;
+    //std::cout << "The calculated eigenvalues is: " << endl << e_val << "\n\n";
+    //std::cout << "Number of iterations: " << iter << "\n\n" ;
+    //std::cout << "Theoretical number of rotations between 3n^2-5n^2: " << 3*pow(n,2)<< "-" << 5*pow(n,2)<< "\n\n";
 
 
     return iter;
@@ -127,20 +153,53 @@ int jacobis_method(arma::mat A,double d, double a, int n){
 
 // This function creates a file which stores the number of iterations
 // with respect to the dimension of the matrix using Jacobi's method
-double iterations_per_dimension(arma::mat A, double d, double a, int n){
+double iterations_per_dimension(arma::mat A, arma::Col<double> d, arma::Col<double> a, int n){
     arma::Col<double> dimensions = arma::vec(int (n/5));
     arma::Col<double> iterations = arma::vec(int (n/5));
     std::ofstream data;
     data.open("iter_per_dim.txt");
     for (int i=0; i<int (n/5); i++){
         dimensions(i) = 5*(i+1);
-        iterations(i) = int(jacobis_method(A,d,a, dimensions(i)));
+        iterations(i) = jacobis_method(A,d,a, dimensions(i));
         std::cout << "Dimension: " << dimensions(i) << "     Iterations: " << iterations(i) << endl;
         data << dimensions(i) << " " << iterations(i) << endl;
     }
     data.close();
 
     return 0;
+}
+
+
+
+// Solving the eigenvalues for schödinger equation using Jacobi's method
+double schrodinger_solver(int N){
+    // some initial conditions
+    double rho_min, rho_N, h;
+    rho_min = 0;
+    rho_N = 1000;
+    h = (rho_N-rho_min)/N;
+    arma::Col<double> rho = arma::linspace(rho_min,rho_N,N);
+    arma::Col<double> V = pow(rho,2);
+    arma::Col<double> d = (2/pow(h,2))+V;
+    arma::Col<double> e = arma::vec(N).ones()*((-1)/pow(h,2));
+    cout << rho << endl << V << endl << d << endl << e << endl;
+
+    // setting up the Schrödinger matrix
+    arma::Mat<double> S = arma::mat(N,N);
+    for (int i=0; i<N-1; i++){
+        S(i,i) = d(i);
+        S(i+1,i) = e(i);
+        S(i,i+1)= e(i);
+    }
+    S(N-1,N-1) = d(N-1);
+
+    jacobis_method(S,d,e,N);
+
+
+
+    return 0;
+
+
 }
 
 
@@ -152,18 +211,18 @@ int main(){
     std::cout << "Size of matrix: ";
     std::cin >> n;
     //int n = 20;
-    int d = 5;
-    int a = 3;
-    arma::Mat <double> A = arma::mat(n,n); A.zeros();
+    arma::Col<double> d = arma::vec(n).ones()*5;
+    arma::Col<double> a = arma::vec(n).ones()*3;
+    arma::Mat <double> A = arma::mat(n,n).zeros();
 
 
     // Setting up the tridiagonal matrix A
     for(int i=0; i<n-1; i++){
-        A(i,i) = d;
-        A(i+1,i) = a;
-        A(i,i+1) = a;
+        A(i,i) = d(i);
+        A(i+1,i) = a(i);
+        A(i,i+1) = a(i);
       }
-    A(n-1,n-1) = d;
+    A(n-1,n-1) = d(n-1);
 
 
     // armadillo and analytical calculation of eigenvalues
@@ -173,7 +232,7 @@ int main(){
     time_t time_arma_1 = time(NULL);
     arma::Col<double> lambs(n);
     for(int j=0; j<n; j++){                         // analytical calculations
-        lambs(j) = lambda_analytical(n, j+1, d, a);
+        lambs(j) = lambda_analytical(n, j+1, d(0), a(0));
     }
     //std::cout << "Comparing the armadillos version of eigenvalues with the analytical solution:" << std::endl
     //     << "Armadillo:" << std::endl << eig_val << std::endl << "Analytic:" << std::endl << lambs << std::endl;
@@ -181,17 +240,23 @@ int main(){
 
     // running the Jacobi's rotational algorithm to find eigenvalues
     time_t time_jaco_0 = time(NULL);
-    jacobis_method(A,d,a,n);
+    //jacobis_method(A,d,a,n);
     time_t time_jaco_1 = time(NULL);
 
+    // Calculating the time taken using armadillo's solver and our algorithm
     double t_arma, t_jaco;
     t_arma = time_arma_1-time_arma_0;
     t_jaco = time_jaco_1-time_jaco_0;
-    std::cout << "Time in seconds using armadillo and Jacobi's method with n = " << n << endl
-              << "Armadillo time: " << t_arma << endl << "Jacobi time: " << t_jaco << endl;
+    //std::cout << "Time in seconds using armadillo and Jacobi's method with n = " << n << endl
+    //          << "Armadillo time: " << t_arma << endl << "Jacobi time: " << t_jaco << endl;
 
     // creating a file for plotting in python
     //iterations_per_dimension(A,d,a,n);
+
+    // solves Schrödinger equation
+    schrodinger_solver(n);
+
+
 
     return 0;
 }
